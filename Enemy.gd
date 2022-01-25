4f4910772a67e8f5c9e8ca2ea9b50bd3e1bd4344
Eduardo M. C. Santos <edumaxsantos@gmail.com>
2022-01-25 23:26:14 +0000

    add enemy duck with minimal movement

--- Enemy.gd
+++ Enemy.gd
@@ -1,16 +1,40 @@
 extends KinematicBody2D
 
+export (int) var speed = 100
+var velocity = Vector2()
+var direction: int
+var move_left: Tween
+var move_right: Tween
 
-# Declare member variables here. Examples:
-# var a = 2
-# var b = "text"
-
-
-# Called when the node enters the scene tree for the first time.
 func _ready():
-	pass # Replace with function body.
+	direction = Utils.Directions.LEFT
+	Utils.change_direction($AnimatedSprite, $CollisionShape2D, direction)
+	var initial_position = self.get_position()
+	move_left = $MoveLeft
+	move_left.interpolate_property(self, "position",
+	initial_position, Vector2(100, 100), 5,
+	Tween.TRANS_LINEAR, Tween.EASE_IN_OUT)
+	
+	
+	move_right = $MoveRight
+	move_right.interpolate_property(self, "position",
+	Vector2(100, 100), initial_position, 5,
+	Tween.TRANS_LINEAR, Tween.EASE_IN_OUT)
+	
+	move_left.start()
+	
 
 
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-#func _process(delta):
-#	pass
+func _on_MoveLeft_tween_completed(object, key):
+	direction = Utils.Directions.RIGHT
+	Utils.change_direction($AnimatedSprite, $CollisionShape2D, direction)
+	move_left.reset(object, key)
+	move_right.start()
+	
+
+
+func _on_MoveRight_tween_completed(object, key):
+	direction = Utils.Directions.LEFT
+	Utils.change_direction($AnimatedSprite, $CollisionShape2D, direction)
+	move_right.reset(object, key)
+	move_left.start()
